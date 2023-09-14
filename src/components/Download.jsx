9a4ed0e381b98e9b8cc6c43407f1a1914459b87b
malikkamal12 <tmalik.k22@gmail.com>
2023-09-14 07:41:12 +0700

    Create Download.jsx

---
+++ src/components/Download.jsx
@@ -0,0 +1,37 @@
+
+import "../styles/Download.css"
+import Statistics from "./Statistics";
+
+
+function Download({data}) {
+    return (
+        <div style={{"width": "100%"}}>
+            <h2 className="text-center">Step 3 - Download</h2>
+
+            { data.outImg && (
+                <div>
+                    <button type="button" className="save-btn" onClick={handleSave}>Save Image</button>
+                    <div className="flex-container">
+                        <div className="output-image-display">
+                            <img src={data.outImg} style={{"maxWidth": "25rem"}} alt="" />
+                        </div>
+                        <Statistics data={data} phase="after" />
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+
+    function handleSave() {
+        if (!data.outImg) {
+            alert("Process an image before save!");
+            return;
+        }
+        let a = document.createElement("a");
+        a.href = data.outImg;
+        a.download = "output";
+        a.click();
+    }
+}
+
+export default Download;
